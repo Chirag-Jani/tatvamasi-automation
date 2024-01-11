c65817b888d275b9d8e97c431a90bf050687ad3e
Chirag Jani <chirag.jani59001@gmail.com>
2024-01-11 21:59:51 +0530

    refactor: code replaced with img in about section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,65 +1,8 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import aboutUsBanner from "../resources/banners/aboutUsBanner.png";
-import RoadMap from "../resources/aboutUsPageImages/RoadMap.png";
-import abt1 from "../resources/aboutUsPageImages/abt1.png";
-import abt2 from "../resources/aboutUsPageImages/abt2.png";
-import abt3 from "../resources/aboutUsPageImages/abt3.png";
-import abt4 from "../resources/aboutUsPageImages/abt4.png";
-import abt5 from "../resources/aboutUsPageImages/abt5.png";
-import abt6 from "../resources/aboutUsPageImages/abt6.png";
-import abt7 from "../resources/aboutUsPageImages/abt7.png";
+import aboutUs from "../resources/aboutUsPageImages/aboutUs.png";
 
 const AboutUs = () => {
-  const data = [
-    {
-      title: "AETC, Surat, Gujarat",
-      desc: "Founded AETC in Surat, Gujarat, India, focusing on Automation and Embedded Training. \n\nEstablished state-of-the-art Automation and Embedded Training Centers, fostering collaborations with engineering colleges.\n\nIntroduced specialized training programs covering PLC, SCADA, drives, and embedded systems for engineering students.",
-      img: abt1,
-    },
-    {
-      title: "Tatvamasi Automation",
-      desc: "Established Tatvamasi Automation to cater to industrial automation needs. Initiated services in automation panel manufacturing, starting with the textile sector and expanding into steel, pharma, chemical, cement, and heavy engineering industries.",
-      img: abt2,
-    },
-    {
-      title: "Government & PSU Sector",
-      desc: `Successfully entered the government and PSU sectors through strategic tender bidding. Introduced specialized training programs covering PLC, SCADA, drives, and embedded systems for engineering students.	Executed notable our 1st projects, including the prestigious "Crane Automation" (40-ton & 20-ton capacity) at Ukai Dam, Gujarat, marking a significant milestone.`,
-      img: abt3,
-    },
-    {
-      title: "Diversification",
-      desc: "Expanded services to include the manufacturing of power panels such as PCC, MCC, and APFC. Became a one-stop solution provider for both automation and electrical sectors.",
-      img: abt4,
-    },
-    {
-      title: "Expansion",
-      desc: "Continued to excel in providing automation and electrical solutions across various industries. Launched an in-house panel enclosure manufacturing unit, ensuring control over quality and customization.",
-      img: abt5,
-    },
-    {
-      title: "Pvt. Ltd. Formation",
-      desc: "Officially transitioned into Tatvamasi Automation Pvt Ltd, reflecting the company's growth and commitment to excellence. Maintained a leading position as an electrical and automation solution provider with a focus on innovation and quality.",
-      img: abt6,
-    },
-    {
-      title: "",
-      desc: "Achieved a significant corporate milestone by establishing a new corporate house. Relocated to a new, state-of-the-art premises, reflecting the company's commitment to growth and modernization. Set new visionary goals to further enhance service quality, innovation, and customer satisfaction.",
-      img: abt7,
-    },
-  ];
-
-  const itemStyle = {
-    width: "80%", // Set the desired width
-    height: "500px", // Set the desired height
-    backgroundColor: "#C3C3C3", // Set the background color
-    clipPath: "polygon(50% 0%, 100% 30%, 100% 100%, 0% 100%, 0% 30%)", // Define the pentagon shape
-    transform: "rotate(-90deg)", // Rotate the pentagon to the correct orientation
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: "50px 0", // Optional margin for spacing between elements
-    borderRadius: "10px",
-  };
   return (
     <Box
       sx={{
@@ -147,54 +90,23 @@ const AboutUs = () => {
           on quality.
         </Typography>
       </Box>
-      <Grid
-        container
+      <Box
         sx={{
+          display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          width: "70%",
-          margin: "auto",
         }}
       >
-        <Grid item xs={4}>
-          <img
-            src={RoadMap}
-            alt="RoadMap"
-            style={{
-              width: "420px",
-              height: "auto",
-            }}
-          />
-        </Grid>
-        <Grid item xs={1}></Grid>
-        <Grid item xs={7}>
-          {data.map((_, index) => (
-            <Box>
-              <Box sx={itemStyle}>
-                <Box
-                  sx={{
-                    width: "70%",
-                    transform: "rotate(90deg)",
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      color: "#162866",
-                      fontSize: "40px",
-                      fontFamily: "Montserrat-Bold",
-                      textAlign: "center",
-                    }}
-                  >
-                    {data[index].title}
-                  </Typography>
-                  <Typography>{data[index].desc}</Typography>
-                </Box>
-              </Box>
-              <img src={data[index].img} alt="" />
-            </Box>
-          ))}
-        </Grid>
-      </Grid>
+        <img
+          style={{
+            width: "70%",
+            height: "auto",
+            margin: "auto",
+          }}
+          src={aboutUs}
+          alt=""
+        />
+      </Box>
     </Box>
   );
 };
